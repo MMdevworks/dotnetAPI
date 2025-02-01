4e62293703c4d18145a6fc690ae142ec4f94de0d
Marie <mmdevq@gmail.com>
2025-01-31 18:31:12 -0800

    db context

--- Data/ApiContext.cs
+++ Data/ApiContext.cs
@@ -1,6 +1,16 @@
-﻿namespace dotnetAPI.Data
+﻿using Microsoft.EntityFrameworkCore;
+using dotnetAPI.Data;
+using dotnetAPI.Models;
+
+namespace dotnetAPI.Data
 {
-    public class ApiContext
+    public class ApiContext : DbContext
     {
+        public DbSet<CareRoom> Occupied { get; set; }
+        public ApiContext(DbContextOptions<ApiContext> options )
+            :base(options)
+        {
+
+        }
     }
 }
